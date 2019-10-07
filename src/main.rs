2270d1e0fe02a880855d56fbb8aaf64f797a294e
Ace Eldeib <alexeldeib@gmail.com>
2019-10-07 04:10:35 -0700

    fix: mod

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::env;
 use std::process;
 
-use minigrep::Config;
+mod lib;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
+    let config = lib::Config::new(&args).unwrap_or_else(|err| {
         println!("problem parsing arguments: {}", err);
         process::exit(1);
     });
@@ -14,7 +14,7 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = lib::run(config) {
         println!("app error: {}", e);
         process::exit(1);
     }
