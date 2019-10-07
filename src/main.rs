74daa24703b8699b210489d888344385d6ac08ed
Ace Eldeib <alexeldeib@gmail.com>
2019-10-07 04:06:08 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
+        println!("problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("app error: {}", e);
+        process::exit(1);
+    }
+}
